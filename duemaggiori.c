22439633504a48a79fb608f7b0b7f299640f3a96
ATruglio <andreat72001@gmail>
2019-03-28 13:52:02 +0100

    Esercizi in c

--- duemaggiori.c
+++ duemaggiori.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
 int main()  {
-	int max,a,i=0;
+	int max,a,i=0,max2;
 	
 	printf("Inserisi dieci numeri \t");
 	scanf("%d", &a);
 	max=a;
-
+	max2=a;
 	while(i<9){
 			printf("\n");
 			scanf("%d", &a);
-			if(a>=max) max=a;
-			i++;	
+			if(a>=max){
+				max2=max;
+				max=a;
+			}
+			i++;
+			/*No 'else' for this 'if'*/
+				
 	}
 
 	printf("Il massimo dei dieci numeri e %d", max);
-
 	putchar('\n');
+	
+	printf("Il secondo massimo dei dieci e %d", max2);
+	putchar('\n');
+
 	return 0;
 }
--- treintre.c
+++ treintre.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+
 int main(){
-int a,d=0;
-printf("Numero di partenza\t");
-scanf("%d",&a);
-printf("\n");
-while(d<5){
-printf("%d\n",a);
-a+=3;
-d++;
-}
-return 0;
+	int a,d=0;
+
+	printf("Numero di partenza\t");
+	scanf("%d",&a);
+	printf("\n");
+
+	while(d<5){
+		printf("%d\n",a);
+		a+=3;
+		d++;
+	}
+
+	return 0;
 
 }
