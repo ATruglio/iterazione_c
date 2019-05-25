84bccd8ad05daa884503a6baad02d2adc7ef11be
ATruglio <andreat72001@gmail.com>
2019-05-25 12:52:12 +0200

    Correzione

--- sommaprodotto.c
+++ sommaprodotto.c
@@ -5,10 +5,8 @@ int main()
 {
 	int cont,somma=0,prodotto=1;
 
-	for(cont=1;cont<=30;cont++){ 
-		if(cont%2==0) {somma+=cont;}
-		else if(cont<15){prodotto*=cont;}
-	}
+	for(cont=2;cont<=30;cont+=2){ somma+=cont;}
+	for(cont=3;cont<=15;cont+=2){ prodotto*=cont;}
 	printf("Somma dei numeri pari: %d\nProdotto dei numeri dispari: %d", somma, prodotto);
 
 	putchar('\n');
